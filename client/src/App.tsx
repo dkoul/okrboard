c515fc19445fcfd08a4acec5542ea747d4457cd7
Anuj Singla <asingla@redhat.com>
2020-05-19 10:05:45 +0530

    code changes.

---
+++ client/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import { Routes } from './components/Routes';
+import { CreateWorkstation } from './components/CreateWorkstation';
+import { CreateObjective } from './components/CreateObjective';
+import { ObrBoards } from './components/ObrBoards';
+import { KeyResults } from './components/KeyResults';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <Routes />
+      <main>
+        <Switch>
+          <Route path={`/create-workstation`} name="createWorkstation" exact={true} component={CreateWorkstation} />
+          <Route path={`/create-objective`} name="createObjective" exact={true} component={CreateObjective} />
+          <Route path={`/create-keyresult`} name="createKeyResult" exact={true} component={KeyResults} />
+          <Route path={`/okr-board`} name="okrBoard" exact={true} component={ObrBoards} />
+          <Redirect to={'/okr-board'} />
+        </Switch>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/ObrBoards.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+// https://diogofcunha.github.io/react-virtualized-tree/#/examples/large-collection
+export function ObrBoards() {
+  return <div>Add tree view for okr board</div>;
+}
